ce8e3ba67b8ebc96a3a885436343ed3f98aa9199
Muhammad Ali Mustaqim <muhammadali55214@gmail.com>
2024-08-02 19:18:51 +0700

    refactor: add auth middleware on merchant route muhammadali55214/exp-4-connect-github-or-gitlab

--- routes/merchant.js
+++ routes/merchant.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const merchantController = require('../controllers/merchantController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/?uid=', merchantController.store); // create new merchant with user id query param
+router.use(authMiddleware);
+
+router.post('/', merchantController.store); // create new merchant with user id query param
 
 module.exports = router;
