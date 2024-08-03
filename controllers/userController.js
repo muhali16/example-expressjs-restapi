99bc3cc23ad8bd6f817f3ce11f58f70f82087462
Muhammad Ali Mustaqim <muhammadali55214@gmail.com>
2024-08-03 13:29:18 +0700

    feat: add change username route
    change username route by user id

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,25 @@ const jsonResponse = require('../utils/jsonResponse');
 
 const index = async (req, res) => {
   const users = await userRepository.all();
-  res.status(200).json(jsonResponse(200, users));
+  res.status(200).json(jsonResponse(200, users ?? "No users found."));
 }
 
 const show = async (req, res) => {
   const {username} = req.params;
   const user = await userRepository.findByUsername(username);
-  res.status(200).json(jsonResponse(200, user));
+  res.status(200).json(jsonResponse(200, user ?? "User not found."));
 }
 
-module.exports = { index, show };
+const changeUsername = async (req, res) => {
+  const { username } = req.body;
+  const changeUsername = await userRepository.updateUserById(
+    req.params.userId,
+      {username},
+  );
+  if (!changeUsername) {
+    res.status(404).json(jsonResponse(404, null, "User not found."));
+  }
+  res.status(200).json(jsonResponse(200, changeUsername));
+};
+
+module.exports = { index, show, changeUsername };
