b758dc150e8399db859497f3699c248664f79ee3
Muhammad Ali Mustaqim <muhammadali55214@gmail.com>
2024-08-03 14:24:50 +0700

    refactor: change user username update  endpoint

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,6 @@ router.use(authMiddleware);
 router.get('/', asyncHandler(userController.index));
 router.get('/:username', asyncHandler(userController.show));
 
-router.post('/change/username/:userId', asyncHandler(userController.changeUsername)); // change username by user Id
+router.post('/:userId/update/username', asyncHandler(userController.changeUsername)); // change username by user Id
 
 module.exports = router;
